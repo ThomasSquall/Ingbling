6496aab2b1307b4340281a4ad1e259f60a7a8346
Thomas Cocchiara <thomas.cocchiara@gmail.com>
2018-12-26 01:43:36 +0100

    Added controllers creation feature to the installer script

--- ingbling.sh
+++ ingbling.sh
@@ -3,6 +3,9 @@
 command="$1"
 repo="https://github.com/ThomasSquall/Ingbling.git"
 dir=$(pwd)/
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
 function check-is-installed {
     command="$1"
@@ -10,7 +13,7 @@ function check-is-installed {
     IS_AVAILABLE=$?
 
     if [[ 0 -ne "IS_AVAILABLE" ]]; then
-        echo "$command is not installed. Please install it before running again"
+        echo "$command is not installed. Please install it before running again."
         exit 1
     fi;
 }
@@ -92,12 +95,11 @@ function init {
         basedir="app"
     fi;
 
-    read -p "URL of your app: " url
+    read -p "URL of your app (http://localhost/$name): " url
 
-    while [[ "" = "$url" ]]; do
-        echo "You cannot leave the URL empty"
-        read -p "URL of your app: " url
-    done;
+    if [[ "" = "$url" ]]; then
+        url="http://localhost/$name"
+    fi;
 
     read -p "Database host (localhost): " db_host
 
@@ -125,7 +127,7 @@ function init {
         read -p "Database password: " db_password
     done;
 
-    echo ""
+    echo
     echo "Name: $name"
     echo "Version: $version"
     echo "Development directory: $basedir"
@@ -135,41 +137,122 @@ function init {
     echo "Database name: $db_name"
     echo "Database user: $db_user"
     echo "Database password: $db_password"
-    echo ""
+    echo
 
-    read -p "Is this configuration ok? " accept
+    read -p "Is this configuration ok? " -n 1 -r
+    echo
 
-    if [[ "" = "$accept" || "y" = "$accept" || "Y" = "$accept" || "yes" = "$accept" || "Yes" = "$accept" ]]; then
+    if [[ ^[Yy]$ =~ "$REPLY" || "" = "$REPLY" ]]; then
         install-repo "$name"
         install-dependencies
         create-config "$name" "$version" "$basedir" "$url" "$db_host" "$db_port" "$db_name" "$db_user" "$db_password"
         basedir-config "$basedir"
+
+        echo
+        echo -e "${GREEN}Congratulations! Your project has been successfully created!${NC}"
+        echo
     else
         read -p "You choose to abort, wanna start over? " restart
         if [[ "" = "$restart" || "y" = "$restart" || "Y" = "$restart" || "yes" = "$restart" || "Yes" = "$restart" ]]; then
             ingbling init
         fi;
-        exit 1
     fi;
+
+    exit 1
+}
+
+function generate-controller {
+    read -p "Controller name? (Home) " name
+
+    if [[ "" = "$name" ]]; then
+        name="Home"
+    fi
+
+    name="$(tr '[:upper:]' '[:lower:]' <<< ${name})"
+    ucName="$(tr '[:lower:]' '[:upper:]' <<< ${name:0:1})${name:1}"
+    cName="$ucName""Controller"
+
+    controller="<?php"
+    controller="$controller"$'\n'
+    controller="$controller"$'\n/**'
+    controller="$controller"$'\n * Class '"$cName."
+
+    if [[ "home" = "$name" ]]; then
+        controller="$controller"$'\n * [\PHPEasyAPI\Server]'
+    else
+        controller="$controller"$'\n * [\PHPEasyAPI\Server("'"$name"'")]'
+    fi
+
+    controller="$controller"$'\n */'
+    controller="$controller"$'\nclass '"$cName"' extends Ingbling\MVC\ControllerBase'
+    controller="$controller"$'\n{'
+    controller="$controller"$'\n    /**'
+    controller="$controller"$'\n     * Initialize here your controller.'
+    controller="$controller"$'\n     */'
+    controller="$controller"$'\n    public function init() { $this->Title = "'"$ucName"'"; }'
+    controller="$controller"$'\n'
+    controller="$controller"$'\n    /**'
+    controller="$controller"$'\n     * Renders the main page of the controller.'
+    controller="$controller"$'\n'"     * [\PHPEasyAPI\Enrichment\Endpoint"'(method = "GET", url = "")]'
+    controller="$controller"$'\n     */'
+    controller="$controller"$'\n    public function getIndex() { $this->render(); }'
+    controller="$controller"$'\n}'
+
+    echo "$controller" > "$1/controllers/$cName.php"
+
+    echo -e "${GREEN}Controller $cName created successfully!${NC}"
+    exit 1
+}
+
+function generate-collection {
+    echo "collections"
 }
 
 function generate {
     if [[ ! -f ingbling.json ]]; then
-        echo "Not an Ingbling project! Please use 'ingbling init' to start one."
+        echo
+        echo -e "${RED}Not an Ingbling project!${NC}"
+        echo "Please use 'ingbling init' to start one."
+        echo
         exit 1
     fi
+
+    basedir=`cat ingbling.json|grep basedir|awk '{$1=$1};1'`
+
+    if [[ "" = "$basedir" ]]; then
+        basedir="app"
+    else
+         basedir="${basedir//\"basedir\": \"}"
+         basedir="${basedir//\",}"
+         basedir="${basedir///}"
+    fi;
+
+    echo "Choose what you want to generate:"
+    echo "[0]: Controller"
+    echo "[1]: Collection"
+    read -p "Enter the corresponding number: " -n 1 -r
+    echo
+
+    case "$REPLY" in
+        0)
+            generate-controller "$basedir"
+            ;;
+        1)
+            generate-collection
+            ;;
+    esac
 }
 
 function guide {
-    echo ""
+    echo
     echo "List of commands:"
-    echo ""
+    echo
     echo "     init | i:    Creates a new Ingbling project"
     echo "      gen | g:    Starts an interactive component generator"
-    echo ""
+    echo
     echo "--version | -v    Shows the Ingbling script version"
     echo "--help    | -h    Shows the guide"
-    echo ""
+    echo
 
     exit 1
 }
